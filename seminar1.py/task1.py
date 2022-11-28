49096f2e2b89569346c6c334bd6a716ae007da12
Руслан Палагнюк <rlpalagniuk@mail.ru>
2022-11-28 18:38:43 +0300

    Добавлен второй вариант решения

--- seminar1.py/task1.py
+++ seminar1.py/task1.py
@@ -6,14 +6,23 @@
 # 25, 5 -> Да
 # 8, 9 -> Да
 
+# Первый вариант решения:
 
-def is_square_namber(first_num, sec_num, start = 1):
-    while start < sec_num:
-        start *= first_num
-    print(start)
-    if (start == sec_num):
-        return True
-    else: 
-        return False
-print(is_square_namber(int(input('Введите первое число: ')), int(input('Введите второе число: '))))
-    
+# def is_square_namber(first_num, sec_num, start = 1):
+   # while start < sec_num:
+     #   start *= first_num
+    # print(start)
+    # if (start == sec_num):
+    #    return True
+    # else: 
+     #   return False
+# print(is_square_namber(int(input('Введите первое число: ')), int(input('Введите второе число: '))))
+
+# Второе решение
+
+num1 = int (input('Введите первое число: '))
+num2 = int (input('Введите второе число: '))
+if num2**2 == num1 or num1**2 == num2:
+    print ('Да')
+else:
+    print ('Нет')
